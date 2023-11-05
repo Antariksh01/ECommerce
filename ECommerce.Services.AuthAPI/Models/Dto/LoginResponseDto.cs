67443513200144565ec8d580495952d465486790
Antariksh Sharma <Antariksha_Sharma@epam.com>
2023-11-05 18:34:58 +0000

    Login Registration Dto's added.

---
+++ ECommerce.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto user { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ ECommerce.Services.AuthAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Services.AuthAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Response { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+    }
+}
