8db9113909398f0193ec4eec2158f79ce8dbaa55
Antariksh Sharma <Antariksha_Sharma@epam.com>
2023-11-07 11:59:01 +0000

    DTO and base url updated in web project.

---
+++ ECommerce/ECommerce.Web/Models/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Web.Models;
+
+public class LoginRequestDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
---
+++ ECommerce/ECommerce.Web/Models/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Web.Models
+{
+    public class LoginResponseDto
+    {
+        public UserDto user { get; set; }
+        public string Token { get; set; }
+    }
+}
