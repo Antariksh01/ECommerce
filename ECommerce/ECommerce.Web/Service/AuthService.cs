afe362874a861e1a5c884bfe56b3b1fc2d7b7e08
Antariksh Sharma <Antariksha_Sharma@epam.com>
2023-11-08 11:49:17 +0000

    Auth service code added in web project.

---
+++ ECommerce/ECommerce.Web/Service/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using ECommerce.Web.Models;
+using ECommerce.Web.Service.IService;
+using ECommerce.Web.Utility;
+
+namespace ECommerce.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private IBaseService _baseService;
+        public AuthService(IBaseService baseService)
+        {
+
+            _baseService = baseService;
+
+        }
+        public async Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            var data = await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.POST,
+                Data = registrationRequestDto,
+                ApiUrl = SD.AuthAPIBase + "/api/auth/AssignRole"
+
+            });
+
+            return data;
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            var data = await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.POST,
+                Data = loginRequestDto,
+                ApiUrl = SD.AuthAPIBase + "/api/auth/login"
+
+            });
+
+            return data;
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            var data = await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.POST,
+                Data = registrationRequestDto,
+                ApiUrl = SD.AuthAPIBase + "/api/auth/register"
+
+            });
+
+            return data;
+        }
+    }
+}
---
+++ ECommerce/ECommerce.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Web.Models;
+
+namespace ECommerce.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        public Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        public Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+        public Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto);
+    }
+}
